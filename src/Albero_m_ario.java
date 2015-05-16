c838dce906f919e3468ba1780128f2dca0afc699
Simone Cappabianca <s.cappabianca@gmail.com>
2015-05-16 19:54:45 +0200

    WIP - Empty version of class

---
+++ src/Albero_m_ario.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Simone Cappabianca - Mat: 5423306
+ *
+ */
+public class Albero_m_ario {
+
+	// Fields
+	
+	// Constructors
+	
+	// Methods
+}
---
+++ src/Nodo_m_ario.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Simone Cappabianca - Mat: 5423306
+ *
+ */
+public class Nodo_m_ario {
+
+	// Fields
+	
+	// Constructors
+	
+	// Methods
+}
